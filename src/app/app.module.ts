5d2b280cf6a4bc87050aa3c69dde86c8d9272cfb
Nick Flynn <nickrflynn@gmail.com>
2018-03-07 23:01:25 -0700

    Initial time, weather, and calendar functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { GoogleApiModule } from 'ng-gapi';
+import { MomentModule } from 'angular2-moment';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TimeComponent } from './time/time.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    CalendarComponent,
+    TimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
